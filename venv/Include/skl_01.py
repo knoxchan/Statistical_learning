076b24eb468ceb65d55cbc990b0e0c113d85b1b5
knox.chan <knox.chan@asiapac.com.cn>
2020-04-28 18:25:26 +0800

    sklearn test

--- venv/Include/skl_01.py
+++ venv/Include/skl_01.py
@@ -79,20 +79,28 @@ def example_03():
 # 实例4 应用 手写数字探索
 def example_04():
     digits = load_digits()
-    fig, axes = plt.subplots(10, 10, figsize=(8, 8), subplot_kw={'xticks': [], 'yticks': []},
-                             gridspec_kw=dict(hspace=0.1, wspace=0.1))
-
-    # axes.flat 一维迭代器
-    for i, ax in enumerate(axes.flat):
-        ax.imshow(digits.images[i], cmap='binary')
-        ax.text(0.05, 0.05, str(digits.target[i]), transform=ax.transAxes, color='green')
-    plt.show()
+    # fig, axes = plt.subplots(10, 10, figsize=(8, 8), subplot_kw={'xticks': [], 'yticks': []},
+    #                          gridspec_kw=dict(hspace=0.1, wspace=0.1))
+    #
+    # # axes.flat 一维迭代器
+    # for i, ax in enumerate(axes.flat):
+    #     ax.imshow(digits.images[i], cmap='binary')
+    #     ax.text(0.05, 0.05, str(digits.target[i]), transform=ax.transAxes, color='green')
+    # plt.show()
 
     X = digits.data
     y = digits.target
 
-    print(X.shape)
-    print(y.shape)
+    from sklearn.manifold import Isomap
+    iso = Isomap(n_components=2)
+    iso.fit(X)
+    data_projected = iso.transform(X)
+
+    plt.scatter(data_projected[:, 0], data_projected[:, 1], c=y, edgecolors='none', alpha=0.5,
+                cmap=plt.cm.get_cmap('Spectral', 10))
+    plt.colorbar(label='digit label',ticks=range(10))
+    plt.clim(-0.5,9.5)
+    plt.show()
 
 
 if __name__ == '__main__':
