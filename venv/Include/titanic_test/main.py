7d8d5d80eaa9fb1fd8b05e4927ceedcbf8f94e67
knox.chan <knox.chan@asiapac.com.cn>
2020-05-12 18:50:29 +0800

    titanic_test

--- venv/Include/titanic_test/main.py
+++ venv/Include/titanic_test/main.py
@@ -8,9 +8,10 @@ from scipy import stats  # for statistics【统计学】
 ''' Plotly visualization '''
 import plotly.graph_objs as go
 from plotly.tools import make_subplots
-from plotly.offline import iplot, init_notebook_mode
 
-init_notebook_mode(connected=True)  # # Required to use plotly offline in jupyter notebook
+# from plotly.offline import iplot, init_notebook_mode
+
+# init_notebook_mode(connected=True)  # # Required to use plotly offline in jupyter notebook
 # 需要再jupyter notebook 上使用plotly离线模式
 
 ''' machine learning models '''  # ensemble 【集成学习】
@@ -39,7 +40,7 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import roc_curve, roc_auc_score
 
-''' Ensembling ''' # Ensembling learning 【集成学习】
+''' Ensembling '''  # Ensembling learning 【集成学习】
 from mlxtend.classifier import EnsembleVoteClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
@@ -48,3 +49,20 @@ from sklearn.ensemble import BaggingClassifier
 from mlens.ensemble import BlendEnsemble
 from vecstack import stacking
 
+''' Customize visualization '''
+plt.style.use('bmh')  # 用bmh风格画图
+sns.set_style({'axes_grid': False})  # removing gridlines 删除网格线
+
+# 导入数据
+''' Read and preview the train data from csv file'''
+train = pd.read_csv('./train.csv')
+print('preview of train data')
+print(train.head(2))
+
+"""Read and preview the test from csv file."""
+test = pd.read_csv('./test.csv')
+print('preview of test data')
+print(test.head(2))
+
+'''merge train and test data together. This eliminates【消除】 the hassle【麻烦】 of handling train and test data seperately for various analysis'''
+merged = pd.concat([train,test],sort=False).reset_index(drop=True)
